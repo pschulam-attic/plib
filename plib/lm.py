feca11b63f52ed340c2965fb372023d2b84b246c
Peter F Schulam <pschulam@gmail.com>
2013-04-02 17:02:01 -0400

    Add ppl function

--- plib/lm.py
+++ plib/lm.py
@@ -4,23 +4,25 @@ Utilities for working with language models.
 
 from itertools import chain as _chain
 
+def ppl(lls, base=10):
+    """
+    Compute the perplexity using the iterable of log likelihoods.
+    """
+    length = len(list(lls))
+    return base ** (- sum(lls) / length)
+
 def parse_srilm_ppl(stream):
     """
     Parse a perplexity file produced by the SRILM toolkit at debugging
     level 2. Log probabilities are given in base 10.
 
-    Returns a 2-tuple, with entries:
+    Returns
 
       (1) List of sentence_scores, where a sentence score is a list of
       tuples that can be destructured using (word, lmword, ll). Word
       is the word from the original sentence, lmword is the word that
       was seen by the LM (could be <unk> for example), and ll is the
       log-likelihood (base 10).
-
-      (2) A dictionary containing summary information from the ppl
-      file. This contains, for example, the total number of sentences,
-      the total number of tokens, the perplexity, and the full
-      log-likelihood.
 
     """
     def parse_word_score(s, WORD=1, LOGP=-2):
@@ -33,15 +35,6 @@ def parse_srilm_ppl(stream):
         word_probs = (parse_word_score(l) for l in lines if l.startswith('\t'))
         return [(w,wlm,ll) for w, (wlm, ll) in zip(sentence, word_probs)]
 
-    def parse_summary(s, NSENTS=2, NWORDS=4, LPROB=3, PPL=5):
-        corpus_summary, model_summary = s.strip().split('\n')
-        nsents = corpus_summary.split()[NSENTS]
-        nwords = corpus_summary.split()[NWORDS]
-        ll = float(model_summary.split()[LPROB])
-        ppl = float(model_summary.split()[PPL])
-        return dict(nsents=nsents, nwords=nwords, ll=ll, ppl=ppl)
-
     chunks = stream.read().strip().split('\n\n')
     sentence_scores = [parse_sentence_score(s) for s in chunks[:-1]]
-    summary = parse_summary(chunks[-1])
-    return sentence_scores, summary
+    return sentence_scores
