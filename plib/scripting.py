575dd01bc14ce16e1e3e2f89ce526301046ec6e8
Peter F Schulam <pschulam@gmail.com>
2013-04-12 09:13:18 -0400

    Remove scripting module

--- plib/scripting.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-def qargs(*args):
-    parser = argparse.ArgumentParser()
-    for arg_name, help_text in args:
-        parser.add_argument(arg_name, help=help_text)
-    return parser.parse_args()
